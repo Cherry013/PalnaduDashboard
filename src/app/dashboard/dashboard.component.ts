1cc50cdf47c063ec16a90dc7e26d032cc038e506
Cherry013 <charan172003gmail.com>
2022-09-28 23:43:06 +0530

    Dashboard Added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  
+  Right = "fa-solid fa-arrow-right";
+  Today_reports = "0"
+  Total_reports = "0"
+  Username = ""
+  Begging = "0";
+  Hotels = "0";
+  pvt = "0";
+  rail = "0";
+  brick = "0";
+  Factories = "0";
+  Construction = "0";
+  other = "0";
+
+
   constructor() { }
 
   ngOnInit(): void {
